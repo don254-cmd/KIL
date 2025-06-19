74697e55d2b91107225fb4b6106255ee3698101f
don254-cmd <KIPTOOCARLOS54@GMAIL.COM>
2025-06-19 16:33:44 +0300

    Add files via upload

---
+++ searchindex.js
@@ -0,0 +1,27 @@
+// search index for WYSIWYG Web Builder
+var database_length = 0;
+
+function SearchPage(url, title, keywords, description)
+{
+   this.url = url;
+   this.title = title;
+   this.keywords = keywords;
+   this.description = description;
+   return this;
+}
+
+function SearchDatabase()
+{
+   database_length = 0;
+   this[database_length++] = new SearchPage("INDEX.html", "Liquor", "All our products undergo thorough testing and quality checks before being dispatched to ensure they meet the highest standards of performance and reliability We are committed to delivering only fully functional and verified items, giving our customers confidence and peace of mind with every purchase  Latest products  Trending today  CUSTOMER SERVICES  SOCIAL MEDIA  COMPANY  The best selection of whisky, vodka and liqueur  Product Name What is the difference between a liquor and a liqueur? Alcohol content  Both liqueurs and liquors have a range of alcohol content Most liquor is in the 40 to 55 percent range of Alcohol by Volume Liqueurs typically contain more ingredients, so the alcohol content is generally lower   ZOOM TECH   HOT AIR MACHINE SCREWS IPHONE   ORDER WITH US  ✅ Fast & Reliable Processing ✅ Nationwide Delivery 📦 Place your order today and receive it within 24 hours – no delays,   💬 Call / WhatsApp  +254 712283827  Copyright © 2025 ZOOM TECH    REFUNDS  All refunds are processed within one week from the date of return, provided the product seal has not been broken or tampered with To qualify for a return, items must be in their original condition and packaging, with all accessories intact Returns that meet these conditions will be reviewed and resolved promptly within the stated period.THANK YOU   HOT AIR MACHINE IPHONE   IPHONE   IPHONE   IPHONE    ", "");
+   this[database_length++] = new SearchPage("SUB.html", "Liquor", "$59.95  Wodka  RELATED PRODUCTS  Lorem ipsum dolor sit amet, consectetur adipiscing elit In ut ullamcorper leo, eget euismod orci Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus Vestibulum ultricies aliquam convallis  Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer nec odio Praesent libero Sed cursus ante dapibus diam Sed nisi Nulla quis sem at nibh elementum imperdiet Duis sagittis ipsum Praesent mauris Fusce nec tellus sed augue semper porta Mauris massa Vestibulum lacinia arcu eget nulla Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos Curabitur sodales ligula in libero  DESCRIPTION  PABLO CARRASCO March 28, 2023 \"Lorem ipsum dolor sit amet, consectetur adipiscing elit Curabitur aliquet quam id dui posuere blandit Vivamus magna justo, lacinia eget consectetur sed, convallis.\" JULIA IGLESIAS April 13, 2023 \"Lorem ipsum dolor sit amet, consectetur adipiscing elit Curabitur aliquet quam id dui posuere blandit Vivamus magna justo, lacinia eget consectetur sed, convallis.\" Product Name Lorem ipsum dolor sit amet, consectetur adipiscing elit Integer nec odio Praesent libero Sed cursus ante dapibus diam Sed nisi Nulla quis sem at nibh elementum imperdiet Duis sagittis ipsum Praesent mauris Fusce nec tellus sed augue semper porta    WHISKEY LIQUEUR CHAMPAGNE RUM LIQUOR CUSTOMER SERVICES  SOCIAL MEDIA  COMPANY  Copyright © 2023 WYSIWYG Web Builder     ", "");
+   this[database_length++] = new SearchPage("REFUNDS.html", "Untitled Page", "FILL UP YOUR REFUND FORM  ", "");
+   this[database_length++] = new SearchPage("TOOLS-ACTIVATION.html", "Untitled Page", " ", "");
+   this[database_length++] = new SearchPage("HELP-AND-CONTACTS.html", "Untitled Page", " ", "");
+   this[database_length++] = new SearchPage("TERMS-AND-CONDITIONS.html", "Untitled Page", " ", "");
+   this[database_length++] = new SearchPage("COMPNY.html", "Untitled Page", " ", "");
+   this[database_length++] = new SearchPage("WHAT-WE-DO.html", "Untitled Page", " ", "");
+   this[database_length++] = new SearchPage("SERVICES.html", "Untitled Page", " ", "");
+   this[database_length++] = new SearchPage("PRIVACY-POLICY.html", "Untitled Page", " ", "");
+   return this;
+}
